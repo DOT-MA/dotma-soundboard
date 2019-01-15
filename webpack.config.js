ccc3a180abc03733055c86d783a950b8c4562453
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-15 23:51:36 +1000

    Setup webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        form: path.resolve(__dirname, "./") + "/src/App.jsx",
+    },
+    output: {
+        filename: "../public/javascript/webpack.bundle.js"
+    },
+    module: {
+        rules: [
+            {
+              loader: "babel-loader",
+              exclude: /node_modules/,
+              query: {
+                presets: ["@babel/preset-react"]
+              }
+            }
+          ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+        modules: ["node_modules", path.resolve(__dirname, "./src")],
+      },
+    mode: "development"
+};
