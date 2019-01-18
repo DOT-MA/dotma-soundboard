39e3f303ac1dfd2ef1672a94cfb095fe48b0074e
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-18 16:59:33 +1000

    Set headers

--- app.js
+++ app.js
@@ -17,16 +17,22 @@ app.use("/dist", express.static(__dirname + "/dist"));
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 
-app.use("/", index);
-
 if (process.env.NODE_ENV !== "test") {
     app.use(logger("dev"));
 }
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use("/", index);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
